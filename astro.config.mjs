04309d58c6829a8fb7fd9d8392048df2914d6d53
Nicholas Valdez <nicholastvaldez@gmail.com>
2025-08-27 13:14:33 -0500

    hook up netlify form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +9,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false
     })
-  ]
+  ],
+  adapter: netlify()
 });
